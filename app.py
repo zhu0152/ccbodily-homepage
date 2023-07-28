b128d196edbe7dc3dd476cceaecbfa4942ab15b0
zhu0152 <134190524+zhu0152@users.noreply.github.com>
2023-07-28 11:16:41 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title('Professional Body Chain Manufacturer For Your Custom Fashion Style')
+    st.text('As “Body chain” keywords founder. Increase your brand value with our custom Bikini, Bra-Chains, Nipple Sticker, Body Chains, Party Dress made with high quality.   ')
+
+    # ... 在此处添加更多的streamlit组件和内容
+
+if __name__ == "__main__":
+    main()
